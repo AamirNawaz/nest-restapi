e77c9a2091670dcadd343533e395fbca58acd353
Aamirkhan <aamir.nawaz990@gmail.com>
2024-02-10 19:30:11 +0500

    jwt applied to login endpoint and token created

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,16 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.register({
+    global:true,
+    secret: "ThisIsMyJwtSecretKey",
+    signOptions: { expiresIn: '60s' },
+  }),
+],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { SignupUserDto } from './dto/signup.user.dto';
 import { LoginUserDto } from './dto/Login.user.dto';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { LoginUserResponseDto } from './dto/login.user.response.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private jwtService :JwtService
   ) {}
 
   async signup(signupUserDto: SignupUserDto): Promise<SignupUserDto> {
@@ -21,17 +23,21 @@ export class UsersService {
     }
   }
 
-  login(loginUserDto: LoginUserDto): LoginUserResponseDto {
+  async login(loginUserDto: LoginUserDto): Promise<LoginUserResponseDto> {
     try {
-        let user = this.userRepository.save(loginUserDto);
-        const TokenResponseDtoObj = new LoginUserResponseDto();
-    
-        //will create token later with jwt here
-        let token = loginUserDto.email;
-        TokenResponseDtoObj.token = token;
+        const user = await this.userRepository.findOneBy({email:loginUserDto.email});
+        if(user?.password !==loginUserDto.password){
+            throw new UnauthorizedException()
+        }
+
+        const TokenResponseDtoObj = new LoginUserResponseDto();       
+        const payload = {sub:user.id,email:user.email}
+        const accessToken = await this.jwtService.signAsync(payload);
+        TokenResponseDtoObj.token = accessToken;
         return TokenResponseDtoObj;    
     } catch (error) {
-        throw new Error("Incorrect email or password!")
+        Logger.debug(error)
+        throw new Error(error)
     }
     
   }
