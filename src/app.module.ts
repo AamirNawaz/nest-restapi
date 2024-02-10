1e00e9c8cd86390c03cd23c231fc40c0154e8bd6
Aamirkhan <aamir.nawaz990@gmail.com>
2024-02-10 16:09:38 +0500

    User model entity created with db connection in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'aamirnawaz',
+      password: 'aamirnawaz',
+      database: 'nest_db',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id:Number
+    @Column()
+    name:String
+
+    @Column()
+    email:String
+
+    @Column()
+    password:String
+
+    @Column({default:false})
+    isActive :Boolean
+    
+}
